719d48d836adb29bd92dc6ef63414e80cce3e49f
AlexanderM <malt.sas@yandex.ru>
2023-06-20 21:50:01 +0300

    Доделал ДЗ семинар 2

--- Program.cs
+++ Program.cs
@@ -104,17 +104,76 @@
 // 9146548 -> 1
 // 3 -> нет
 
+// Console.WriteLine("Введите число для проверки");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// if (num1 < 10)
+// Console.WriteLine("Такой цифры нет");
+// else
+// {
+//     // int num2 = 0;
+//     while (num1 >= 99)
+//     {
+//         num1 = num1 / 10;
+//     }
+//     num1 = num1 % 10;
+//     Console.WriteLine(num1);
+// }
+
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на 
+//выходе показывает вторую цифру этого числа. Через строку решать нельзя.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// Console.WriteLine("Введите число для проверки");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// if (num1 < 1000 & num1 > 99 )
+// {
+//     num1 = num1 / 10;
+//     num1 = num1 % 10;
+//     Console.WriteLine($"Второе число равно {num1}");
+// } 
+// else
+//     Console.WriteLine("Введите трёхзначное число для проверки");
+
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.Через строку решать нельзя.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
 Console.WriteLine("Введите число для проверки");
 int num1 = Convert.ToInt32(Console.ReadLine());
-if (num1 < 10)
-Console.WriteLine("Такой цифры нет");
+if (num1 < 99 )
+Console.WriteLine("В числе нет третьей цифры");
 else
 {
-    // int num2 = 0;
-    while (num1 >= 99)
+    while (num1 > 999 ) 
     {
         num1 = num1 / 10;
     }
-    num1 = num1 % 10;
-    Console.WriteLine(num1);
+    Console.WriteLine(num1 % 10);
 }
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// Console.WriteLine("Введите число недели для проверки");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// if (num1 > 7 )
+// Console.WriteLine("Введённое число не корректное");
+// else
+// {
+//     if (num1 < 6)
+//     Console.WriteLine("Этот день будний");
+//     else
+//     Console.WriteLine("Этот день выходной");
+// }
